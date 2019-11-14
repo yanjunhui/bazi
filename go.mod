148deee4c0cc2d8c02c55713cc1618d3927b36c4
YanJunhui <i@yanjunhui.com>
2019-11-14 20:20:16 +0800

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module bazi
+
+go 1.13
---
+++ sizhu.go
@@ -0,0 +1,101 @@
+package bazi
+
+import (
+	"fmt"
+	"time"
+)
+
+// 补充五行
+func CalcWuXing(pZhu *TZhu) TZhu {
+	// 五行
+	Get5XingFromGan2(&pZhu.Gan.WuXing, pZhu.Gan.Value)
+	Get5XingFromZhi2(&pZhu.Zhi.WuXing, pZhu.Zhi.Value)
+	return *pZhu
+}
+
+// 从八字年获得年柱
+func GetZhuFromYear(nYear int) TZhu {
+	var zhu TZhu
+	// 获得八字年的干支，0-59 对应 甲子到癸亥
+	GetGanZhiFromYear2(&zhu.GanZhi, nYear)
+	// 获得八字年的干0-9 对应 甲到癸
+	// 获得八字年的支0-11 对应 子到亥
+	ExtractGanZhi2(&zhu.GanZhi, &zhu.Gan, &zhu.Zhi)
+
+	return CalcWuXing(&zhu)
+}
+
+// 从八字月 和 年干 获得月柱
+func GetZhuFromMonth(nMonth int, nGan int) TZhu {
+	nMonth = nMonth % 12
+	nGan = nGan % 10
+	if nMonth <= 0 {
+		nMonth += 12
+	}
+
+	if nGan < 0 {
+		nGan += 10
+	}
+
+	var zhu TZhu
+	// 根据口诀从本年干数计算本年首月的干数
+	switch nGan {
+	case 0, 5:
+		// 甲己 丙佐首
+		nGan = 2
+	case 1, 6:
+		// 乙庚 戊为头
+		nGan = 4
+	case 2, 7:
+		// 丙辛 寻庚起
+		nGan = 6
+	case 3, 8:
+		// 丁壬 壬位流
+		nGan = 8
+	case 4, 9:
+		// 戊癸 甲好求
+		nGan = 0
+	}
+
+	// 计算本月干数
+	nGan += ((nMonth - 1) % 10)
+
+	zhu.Gan.Value = nGan % 10
+	zhu.Zhi.Value = (nMonth - 1 + 2) % 12
+
+	CombineGanZhi2(&zhu.GanZhi, &zhu.Gan, &zhu.Zhi)
+
+	return CalcWuXing(&zhu)
+}
+
+// 从公历天 获得日柱
+func GetZhuFromDay(nYear, nMonth, nDay, nHour int) TZhu {
+	var zhu TZhu
+
+	date, err := time.Parse("2006-01-02", fmt.Sprintf("%d-%.2d-%.2d", nYear, nMonth, nDay))
+	if err == nil && nHour == 23 {
+		date = date.AddDate(0, 0, 1)
+		nYear = date.Year()
+		nMonth = int(date.Month())
+		nDay = date.Day()
+	}
+
+	// 通过总天数 计算当前天的一个值
+	zhu.GanZhi.Value = GetGanZhiFromDay(GetAllDays(nYear, nMonth, nDay))
+
+	// 获得八字日的干0-9 对应 甲到癸
+	// 获得八字日的支0-11 对应 子到亥
+	ExtractGanZhi2(&zhu.GanZhi, &zhu.Gan, &zhu.Zhi)
+
+	return CalcWuXing(&zhu)
+}
+
+// 从公历小时,  获得日柱天干获取时柱
+func GetZhuFromHour(nHour int, nGan int) TZhu {
+	var zhu TZhu
+
+	zhu.Gan.Value, zhu.Zhi.Value = GetGanZhiFromHour(nHour, nGan)
+	CombineGanZhi2(&zhu.GanZhi, &zhu.Gan, &zhu.Zhi)
+
+	return CalcWuXing(&zhu)
+}
