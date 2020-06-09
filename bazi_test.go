1b1af3b4a5df8f56fd7c09ded0d5c1add268c777
YanJunhui <i@yanjunhui.com>
2020-06-09 16:57:47 +0800

    update

--- bazi_test.go
+++ bazi_test.go
@@ -1,15 +1,1 @@
 package bazi
-
-import (
-	"fmt"
-	"testing"
-)
-
-func TestGetBazi(t *testing.T) {
-	bz := GetBazi(2020,5,31,23,30,0,0)
-
-	fmt.Println(bz.SiZhu.YearZhu.GanZhi.ToString())
-	fmt.Println(bz.SiZhu.MonthZhu.GanZhi.ToString())
-	fmt.Println(bz.SiZhu.DayZhu.GanZhi.ToString())
-	fmt.Println(bz.SiZhu.HourZhu.GanZhi.ToString())
-}
